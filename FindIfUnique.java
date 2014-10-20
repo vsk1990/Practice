fcd1ebcd563167619566510c4e5954bff034556d
Vaibhav Suresh Kumar <vaibhav.sureshkumar@gmail.com>
2014-10-19 20:15:46 -0400

    find Unique string complete

--- FindIfUnique.java
+++ FindIfUnique.java
@@ -5,14 +5,26 @@ class FindIfUnique{
     private HashMap<Character,Boolean> storeHouse = new HashMap<Character,Boolean>();
 
     public int check(String s){
-   
+  
+
+
+        long time1 = System.nanoTime();
         for(int i = 0; i< s.length(); i++){
         
              char c = s.charAt(i);
              if(storeHouse.containsKey(c)){
         
-                return -1;
+        long time2 = System.nanoTime();
+                
         
+        System.out.println("Time: "+(time2-time1));
+        return -1;
+
+
+        
+
+        
+
              }
              else{
 
@@ -22,7 +34,11 @@ class FindIfUnique{
     
 
         }
+
+
+
     
+
         return 0;
     }
 
@@ -30,11 +46,44 @@ class FindIfUnique{
 
 class FindIfUniqueInPlace{
 
-    public int check(){
+    public int check(String s){
     
+       
+
+       
+        int length = s.length();
+        int result = 0;
+        Integer[] c = new Integer[length];
+        
+        long time1 = System.nanoTime();
         //Quick sort and traverse
-        return 0;
-    
+        
+        for(int i = 0; i< length; i++){
+       
+            c[i] = Character.getNumericValue(s.charAt(i));
+        
+        }
+
+        QuickSort q = new QuickSort();
+        
+        q.sort(c,0,length-1);
+
+        for(int i = 1; i< length; i++){
+            
+            if(c[i]==c[i-1]){
+            
+                result = -1;
+            
+            }
+        }
+
+
+
+        long time2 = System.nanoTime();
+        
+        System.out.println("Time: "+(time2-time1));
+        return result;
+            
     
     }
 
@@ -45,8 +94,10 @@ class MainClass{
 
     public static void main(String[] args){
     
-        FindIfUnique findObj = new FindIfUnique();
+
+        FindIfUniqueInPlace findObj = new FindIfUniqueInPlace();
         
+        //FindIfUnique findObj = new FindIfUnique();
         int i = findObj.check("Helo");
         if(i==0){
             
@@ -62,5 +113,3 @@ class MainClass{
     }
 
 }
-
-
