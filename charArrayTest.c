fbe8f6ac717c2edee50fc1d0325ecdaae0ffa22c
Vaibhav Suresh Kumar <vaibhav.sureshkumar@gmail.com>
2014-10-23 03:14:24 -0400

    Sample code to check if a char is returned  when int is passed to char

---
+++ charArrayTest.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+
+
+int main(){
+
+
+
+    char a[10];
+
+    for(int i = 0; i<10; i++){
+    
+        a[i] = 'a';
+    
+    
+    }
+
+    for(int i = 0 ;i<10 ;i++){
+    
+        printf("Char %c and %d", a[i],a[i]);
+    
+    }
+
+
+}
